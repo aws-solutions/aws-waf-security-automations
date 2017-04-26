34781f9f55bb9844ab9b87585f696065b741f484
Jamie Starke <jamies@giftbit.com>
2017-04-26 10:15:29 -0700

    Changes lambda functions to get the StackName from the StackName environment variable for more consistency, and longer value support

--- code/custom-resource/custom-resource.py
+++ code/custom-resource/custom-resource.py
@@ -17,6 +17,7 @@ import requests
 import datetime
 from urllib2 import Request
 from urllib2 import urlopen
+import os
 
 print('Loading function')
 
@@ -432,7 +433,10 @@ def lambda_handler(event, context):
     responseData = {}
     try:
         cf = boto3.client('cloudformation')
-        stack_name = event['ResourceProperties']['StackName']
+        stack_name = os.environ.get('StackName')
+
+        print "stack_name: %s" % stack_name
+
         cf_desc = cf.describe_stacks(StackName=stack_name)
 
         request_type = event['RequestType'].upper()
